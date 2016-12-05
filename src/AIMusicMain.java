530df44aaeb82ccad93d955c9c5fc93496a3b402
Andreína Vivas Thomas <avivasthomas@gmail.com>
2016-12-04 20:04:56 -0500

    Update AIMusicMain.java

--- src/AIMusicMain.java
+++ src/AIMusicMain.java
@@ -2,6 +2,7 @@ import org.jfugue.player.Player;
 
 public class AIMusicMain {
     public static void main(String[] args){
+        //create new Player
         Player musicPlayer = new Player();
     }
 }
